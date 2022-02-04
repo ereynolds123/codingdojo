e176e6d58976a3260c728ad9de765ec655b7d1b1
Clifford Reynolds <ereynold@estee.com>
2022-02-04 17:01:36 -0500

    All Lisa song ids have been added to an array

--- Lisas of Spotify/lisa.py
+++ Lisas of Spotify/lisa.py
@@ -3,9 +3,12 @@ import json
 with open("lisas.json") as JsonFile:
     data= json.load(JsonFile)
     jsonData= data["tracks"]["items"]
+    tracksArr =[]
 
     for trackId in jsonData:
         values = trackId["id"]
+        tracksArr.append(values)
 
         
         print(values)
+    print(tracksArr)
